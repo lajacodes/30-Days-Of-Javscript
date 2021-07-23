2dacaee6cfdbbaaeb9cf43bb9b1aaacd3766e1d8
Goodness Adelaja <lajaololade@gmail.com>
2021-07-23 15:18:49 +0100

    feat:Javascript exercises

---
+++ 01_ Introduction/Variabl.js
@@ -0,0 +1,26 @@
+   let myName = 'laja';
+  let isGettingMarried = false;
+  let dontTouch;
+  let goingToBenin = null;
+ 
+ let myStick;
+ let isRunning;
+ let Ticktok;
+ let rightClick;
+
+ let myStuff ='red';
+ let isGoing = true;
+ let TickPop ='dred';
+ let isRightLeft = false;
+
+ let firstName  
+ let lastName 
+ let maritalStatus 
+ let myCountry 
+  let myAge 
+
+ let firstNamee = 'laja', lastNamee = 'codes', maritalStatuss = 'single', myCountryy = 'nigeria', _myAgee = 25;
+
+ let _myAge = 25, yourAge = 30;
+
+ console.log ("i am " + _myAge + " years old.");
---
+++ 01_ Introduction/datatype.js
@@ -0,0 +1,9 @@
+let myName = 'laja';
+let isGettingMarried = false;
+let dontTouch ='top';
+let goingToBenin = null;
+
+console.log(typeof 'laja');
+console.log(typeof false);
+console.log(typeof 'top');
+console.log(typeof null); 
