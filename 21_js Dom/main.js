bdf4361bbe22fe53086fc9e2fe073b1ee8caddf7
Goodness Adelaja <lajaololade@gmail.com>
2021-08-07 16:27:23 +0100

    feat:first js_dom

---
+++ 21_js Dom/main.js
@@ -0,0 +1,100 @@
+/*const paragragh = document.querySelector('p')
+ console.log(paragragh);
+
+
+ let para1 = document.querySelector('#first')
+ console.log(para1);
+ let para2 = document.querySelector('#second')
+ console.log(para2);
+ let para3 = document.querySelector('#third')
+ console.log(para3);
+ let para4 = document.querySelector('#fourth')
+ console.log(para4);
+
+
+
+const nodelist = document.querySelectorAll('p')
+console.log(nodelist);
+
+const newArray = []
+const nodelistr = nodelist.forEach((list) => {
+  newArray.push(list.textContent) 
+})
+console.log(newArray);
+
+const getItems = document.querySelectorAll('p')
+getItems[3].textContent = ('Let play !!')
+
+
+getItems[0].className = 'little'
+getItems[1].className = 'little'
+getItems[2].className = 'little'
+getItems[3].className = 'little'
+     getItems[0].id=  'first'
+     getItems[1].id =  'second'
+     getItems[2].id =  'third'
+     getItems[3].id = 'fourth'
+
+console.log(getItems);
+
+
+
+getItems[0].style.color = 'red'
+getItems[1].style.color = 'blue'
+getItems[2].style.color = 'orange'
+getItems[3].style.color = 'green'
+getItems[0].style.background = 'black'
+getItems[1].style.background = 'grey'
+getItems[2].style.background = 'purple'
+getItems[3].style.background = 'black'
+getItems[0].style.border = '2px solid green'
+getItems[1].style.border = '2px solid yellow'
+getItems[2].style.border = '2px solid red'
+getItems[3].style.border = '2px solid tomato'
+getItems[0].style.fontSize = '4rem'
+getItems[1].style.fontSize = '4rem'
+getItems[2].style.fontSize = '4rem'
+getItems[3].style.fontSize = '4rem'
+getItems[0].style.fontFamily = 'sanSeriff'
+getItems[1].style.fontFamily = 'sanSeriff'
+getItems[2].style.fontFamily = 'sanSeriff'
+getItems[3].style.fontFamily = 'sanSeriff'
+
+
+
+
+//  Select all paragraphs and loop through each elements and give the first and third paragraph a color of green, and the second and the fourth paragraph a red color 
+
+// const geter = document.querySelectorAll('p')
+// const emptyArr = []
+
+nodelist.forEach((item,index) => {
+      item.style.fontSize = '2rem'
+      item.style.fontFamily = 'monospace'
+    if (index % 2 )
+    {
+         item.style.color = 'green'
+    }
+    else{
+         item.style.color = 'red'
+    }
+})*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
